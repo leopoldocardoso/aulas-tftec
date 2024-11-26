ebc767387e699fc80b17ccaa584576ad1ea9087f
Leopoldo Peixoto <peixoto.leopoldo@outlook.com>
2024-11-26 08:55:58 -0300

    First Commit

--- mod08/lab03/backend.tf
+++ mod08/lab03/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "stiacterraformstate"
     container_name       = "aulas-terraform"
     key                  = "mod08-lab03-terraform.tfstate"
-    access_key           = "YrdzxV2sZi+smc8zKYNVusZrDsX4DsiA6ViE8ckSeQxRUXcsOgUwd27pu5+j2ibBbwKZsMxe/P6a+AStioIcNA=="
+   
   }
 }
