eaa690627750a7f69066ba5c9b8e3fdbbce5cd97
Leopoldo Peixoto <peixoto.leopoldo@outlook.com>
2024-11-26 08:55:29 -0300

    First Commit

--- mod08/lab03/provider.tf
+++
@@ -1,9 +0,0 @@
-# Terraform Provider Block
-provider "azurerm" {
-  features {}
-
-  subscription_id = "2783b349-a076-405b-b47a-90095f330d7c"
-  client_id       = "1dabbf8d-b5b0-4ef5-9c35-d35a51361f4f"
-  client_secret   = "W6w8Q~vkqPQahGyj9my_1sk5aJdHyDjc1idlAbmq"
-  tenant_id       = "0497384a-7ab4-4428-a12a-aff6c88bbe8b"
-}
