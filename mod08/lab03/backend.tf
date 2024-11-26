17e3677c2d309c31f3f0efb64c71f3066bed85c1
Leopoldo Peixoto <peixoto.leopoldo@outlook.com>
2024-11-26 08:56:34 -0300

    First Commit

--- mod08/lab03/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-iac-terraform"
-    storage_account_name = "stiacterraformstate"
-    container_name       = "aulas-terraform"
-    key                  = "mod08-lab03-terraform.tfstate"
-   
-  }
-}
